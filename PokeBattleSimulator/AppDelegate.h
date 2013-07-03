a96a871c8b50b3feed8492e61d8de9b0e1ef7d15
Fukutaro Hori <tivrsky@gmail.com>
2013-07-04 01:50:13 +0900

    Initial Commit

---
+++ PokeBattleSimulator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PokeBattleSimulator
+//
+//  Created by fukutarohori on 2013/07/04.
+//  Copyright (c) 2013年 Fukutaro Hori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PokeBattleSimulator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PokeBattleSimulator
+//
+//  Created by fukutarohori on 2013/07/04.
+//  Copyright (c) 2013年 Fukutaro Hori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
